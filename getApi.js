670135b59cfa333c73d363ac577bcb9a7d317ff4
Hoan Tran <hoantran.us@gmail.com>
2019-01-23 23:54:13 +0700

    sdfsdf

--- getApi.js
+++ getApi.js
@@ -1,8 +1,17 @@
+var HostUrl = "http://admin.codeforfun.club/";
+var ApiUrl = HostUrl + "api/";
+var ImgHost = HostUrl + "storage/";
+
+// API
+var ApiGetSiteSetting = ApiUrl + "get_site_setting";
+var ApiGetAllPosts = ApiUrl + "get_all_posts";
+var ApiGetPostDetail = ApiUrl + "get_post_detail?slug=lorem-ipsum-post";
+
 $(document).ready(function(){
   console.log('Load Page');
   //Load page
   //load get_site_setting
-  $.get("https://admin.codeforfun.club/api/get_site_setting", function(responseData) {
+  $.get(ApiGetSiteSetting, function(responseData) {
     
     //phan hien thi categories
     var categories = responseData.data.categories;
@@ -16,7 +25,6 @@ $(document).ready(function(){
 
     //load setting
     var settingPrf = responseData.data.settings;
-    console.log(settingPrf);
     var profilHtml = '';
     var avatarHtml = '';
     for(var i = 0; i < settingPrf.length; i++){
@@ -30,18 +38,21 @@ $(document).ready(function(){
     }
     $('#profile_setting').html(profilHtml);
     $('#avatar_setting').html(avatarHtml);
+    $('#avatar-article-header').html(avatarHtml);
+    $('#avatar-article-mobile').html(avatarHtml);
     //end load setting
   });
   //and load get_site_setting
 
   //load get_all_posts
-  $.get("https://admin.codeforfun.club/api/get_all_posts", function(responseData){
+  $.get(ApiGetAllPosts, function(responseData){
     var posts = responseData.data.data;
     var postHtml = '';
+    console.log(posts);
     for(var i = 0; i < posts.length; i++){
       postHtml += `<div class="post">
                       <div class="img_post">
-                        <div class="background_post" style="background-image:url(`+posts[i].image+`)"></div>
+                        <div class="background_post" style="background-image:url('`+ImgHost + posts[i].image+`')"></div>
                         <div class="content_post">
                           <p>`+posts[i].excerpt+`</p>
                           <span>Real More</span>
@@ -67,6 +78,28 @@ $(document).ready(function(){
   //end load page
 
   //load chi tiet bai viet
-
+  $.get(ApiGetPostDetail, function(responseData){
+    console.log(responseData);
+    var articleContent = responseData.data;
+    var articleHtml = `<div class="content_between">
+        <div class="content_between_left">
+          <div class="image_content_between">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/751678/skytsunami.png" alt="">
+          </div>
+          <div class="profile_content_between_left">
+            <span>@michaelhoan</span>
+            <p>Published `+articleContent.updated_at+`</p>
+          </div>
+        </div>
+        <h2 class="title_article">`+articleContent.title+`</h2>
+        <div class="image_article_content">
+          <img src="`+ ImgHost + articleContent.image+`" alt="">
+        </div>
+        <div class="article_content_body">
+          `+articleContent.body+`
+        </div>
+      </div>`;
+    $('#test_api').html(articleHtml);
+  });
   //end load chi tiet bai viet
 });
