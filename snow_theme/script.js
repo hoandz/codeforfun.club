923646262cff61ac5731185b257f3b83dd7d103b
Hoan Tran <hoantran.us@gmail.com>
2019-02-20 23:55:48 +0700

    asdfadsf

--- snow_theme/script.js
+++ snow_theme/script.js
@@ -44,5 +44,17 @@ $(document).ready(function(){
     arrows: false,
   });
   // end post_latest
-
+  //xu ly one page
+  var body = $('html, body');
+  $('.header-right ul li:nth-child(1) a').click(function(event){
+    //ham event.preventDefault ngan chan the a load trang
+    event.preventDefault();
+    body.animate({ scrollTop: $('.chapter1').offset().top}, 1000, 'swing');
+  });
+  $('.header-right ul li:nth-child(2) a').click(function(event){
+    //ham event.preventDefault ngan chan the a load trang
+    event.preventDefault();
+    body.animate({ scrollTop: 3800}, 1000, 'swing');
+  });
+  // end xu ly one page
 });
